424bb737dfbf544233f642f8c1e28c2ace9e28b9
Mohamed Guerroui <guerroui.m@gmail.com>
2020-11-05 17:07:04 +0100

    ajout du fichier lightboxImage

--- src/components/lightbox/Lightbox.vue
+++ src/components/lightbox/Lightbox.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="lightbox" v-if="image">
-        <img :src="image">
+        <lightbox-image :image="image"></lightbox-image>
     </div>
 </template>
 
 <script>
 import './LightboxDirective'
+import LightboxImage from './LightboxImage'
 import store from './LightboxStore'
 export default {
+    components: {
+        LightboxImage
+    }
     data () {
         return {
             state: store.state
---
+++ src/components/lightbox/LightboxImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>
+            chargement ...
+        </div>
+        <img>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+        }
+    }
+}
+</script>
+
+<style></style>
