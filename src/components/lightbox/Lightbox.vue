c0d8ccb6c0252c307427e40872913e2354b787c3
Mohamed Guerroui <guerroui.m@gmail.com>
2020-11-07 14:39:55 +0100

    ajout des flèches de défilement vers la droite et vers la gauche

--- src/components/lightbox/Lightbox.vue
+++ src/components/lightbox/Lightbox.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="lightbox" v-if="image" @click="close">
-        <lightbox-image :image="image"></lightbox-image>
-        <div class="lightbox__close"></div>
+        <transition :name="transition">
+            <lightbox-image :image="image" :key="image"></lightbox-image>
+        </transition>
+        <div class="lightbox__close" @click="close"></div>
+        <div class="lightbox__btn lightbox__next" @click.stop.prevent="next"></div>
+        <div class="lightbox__btn lightbox__prev" @click.stop.prevent="prev"></div>
     </div>
 </template>
 
@@ -13,18 +17,30 @@ export default {
     components: { LightboxImage },
     data () {
         return {
-            state: store.state
+            state: store.state,
+            direction: 'next'
         }
     },
     methods: {
-        close () { store.close() }
+        close () { store.close() },
+        next () { 
+            this.direction = 'next'
+            store.next() 
+            },
+        prev () { 
+            this.direction = 'prev'
+            store.prev() 
+            }
     },
     computed: {
         image () { 
             if (this.state.index !== false) {
                 return this.state.images[this.state.index]
             }
-         }
+        },
+        transition () {
+            return 'lightbox-' + this.direction
+        }
     }
 }
 </script>
